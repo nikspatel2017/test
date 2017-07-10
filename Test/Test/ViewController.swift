c946da1636f0392fbc0a34b67ea281d4ae11ba99
AppDeveloper <nikesh.handstogether@gmail.com>
2017-07-10 17:20:04 +0530

    Add project

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by AppDeveloper on 10/07/17.
+//  Copyright © 2017 AppDeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var temp : String = "Test PR app"
+    
+    var button : UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.layoutIfNeeded()
+        
+        print("temp")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
